cda1a21a4807705959500b84753a66cfe54d165d
asperbaawp <asperbaawp@gmail.com>
2024-07-19 10:56:31 -0500

    July 19th Website Alpha
    Template of the site with basic layout, etc. July 19th.

---
+++ July 19/wp-content/uploads/uag-plugin/assets/1000/uag-js-1090.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-bik1knvf' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-hy5o4ebb' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-9tswhmfc' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-ijp6p4q9' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-sso1w5fo' );
+});
+ });
